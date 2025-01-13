ec7fc1d2a00680444b2dd074283fd4735c936b2b
Claudias-maths <claudia21Schreiber@gmail.com>
2025-01-13 15:06:11 -0500

    1.4.0 userInput.py updates
    plays nice with the 1.4.0 dependency checks now.

--- userInput.py
+++ userInput.py
@@ -7,18 +7,6 @@ if __name__ == "__main__":
     f = open("Input.txt", "w")
     f.write("")
     f.close()
-    '''while main:
-        with open("Output.txt") as f:
-            for x in f:
-                if x == "go":
-                    main = False
-    with open("Jukebox.txt") as g:
-        for x in g:
-            if bool:
-                print(x)
-                bool = False
-            else:
-                bool = True'''
     main = True
     clean = False
     with open("Output.txt", "w") as f:
@@ -28,29 +16,36 @@ if __name__ == "__main__":
             for x in f:
                 if x == "done":
                     main = False
+                    kill = False
+                if x == "done. failed.":
+                    main = False
+                    kill = True
 
-    time.sleep(0.25)
-    main = True
-    f = open("Output.txt", "w")
-    f.write("")
-    f.close()
-    while main:
-        time.sleep(.1)
-        usr_input = input("")
-        if usr_input == "dev.killall":
-            main = False
-            usr_input = "quit"
-        f = open("Input.txt", "w")
-        f.write(usr_input)
+    if not kill:
+        time.sleep(0.25)
+        main = True
+        f = open("Output.txt", "w")
+        f.write("")
         f.close()
-        time.sleep(.1)
-        f = open("Output.txt", "r")
-        for x in f:
-            if "quit" in x:
+        while main:
+            time.sleep(.1)
+            usr_input = input("")
+            if usr_input == "dev.killall":
                 main = False
-            '''if "clean" in x:
-                clean = True
-        if clean:
-            clean = False'''
-        f.close()
+                usr_input = "quit"
+            f = open("Input.txt", "w")
+            f.write(usr_input)
+            f.close()
+            time.sleep(.1)
+            f = open("Output.txt", "r")
+            for x in f:
+                if "quit" in x:
+                    main = False
+                '''if "clean" in x:
+                    clean = True
+            if clean:
+                clean = False'''
+            f.close()
+    else:
+        time.sleep(2)
 
